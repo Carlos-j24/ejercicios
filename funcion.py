065795da60c5bfdcecf7c3a6df23b0473eae41d4
Carlos-j24 <129704086+Carlos-j24@users.noreply.github.com>
2023-04-02 20:40:49 -0500

    Add files via upload

---
+++ funcion.py
@@ -0,0 +1,13 @@
+# Escribir una función a la que se le pase una cadena <nombre> y muestre por pantalla el saludo ¡hola <nombre>!.
+
+def saludo (nombre):
+   
+    print('Hola ' + nombre)
+    
+
+saludo ('Carlos')
+saludo ('Edilson')
+saludo ('Juan')
+
+
+
